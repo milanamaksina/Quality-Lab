fd1928c31aeaca1b70a11f86dca73dfebec8761c
m_maksina <m_maksinaa@mail.ru>
2023-01-21 19:58:34 +0300

    feat: abstractPage and support

---
+++ Quality-Lab/PageObjects/AbstractPage.cs
@@ -0,0 +1,15 @@
+﻿using QualityLab.Support;
+
+namespace QualityLab.PageObjects
+{
+    public class AbstractPage
+    {
+        protected IWebDriver _driver;
+
+        public AbstractPage()
+        {
+            _driver = SingleWebDriver.GetInstance();
+            _driver.Manage().Window.Maximize();
+        }
+    }
+}
---
+++ Quality-Lab/Support/Urls.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QualityLab.Support
+{
+    public class Urls
+    {
+        public const string Host = $"https://mail.ru/";
+    }
+}
